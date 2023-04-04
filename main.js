1095b1993146118911b4c4ee5fe6e96c7bd76d25
Luis <lemt.09@gmail.com>
2023-04-04 17:31:43 -0500

    Integrando menu mobile y ajustando media query

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const navEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
+const mobileMenuIcon = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 navEmail.addEventListener("click", toggleDesktopMenu);
+mobileMenuIcon.addEventListener("click", toggleMobileMenu);
+
 
 function toggleDesktopMenu()
 {
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu()
+{
+    mobileMenu.classList.toggle("inactive");
 }
